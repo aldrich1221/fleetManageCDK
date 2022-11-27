8ec1bef28e04ca53c091475bc0fd9241956fc291
User <aldrich1221@gmail.com>
2022-11-28 00:22:17 +0800

    image lambda example

--- basicConstruct/lib/basic_construct-stack.ts
+++ basicConstruct/lib/basic_construct-stack.ts
@@ -1,13 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as resourcegroups from 'aws-cdk-lib/aws-resourcegroups';
+import * as path from 'path';
 
 export class BasicConstructStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const lambdaFunc = new lambda.DockerImageFunction(
+      this,
+      'CodepipelineEventLambda',
+      {
+        code: lambda.DockerImageCode.fromImageAsset(
+          path.join(__dirname, 'lambda-source','source'),
+          {
+            cmd: ['app.handler'],
+          }
+        ),
+       
+      }
+    );
+
+    // 監聽 CloudTrail 在 IAM 新建 user 時觸發
+
+
 
     // example resource
     const queue = new sqs.Queue(this, 'BasicConstructQueue', {
@@ -34,5 +54,7 @@ export class BasicConstructStack extends cdk.Stack {
       includeResourceTypes: ['includeResourceTypes'],
       priority: 123,
     });
+
+
   }
 }
