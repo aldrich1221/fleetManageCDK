f3d6b0660b32bc8c06307df2f90f444f59f8af92
Aldrich Chen(陳子權) <Aldrich_Chen@HTC.COM>
2022-11-30 17:54:11 +0800

    netowrk

--- API/apiConstruct/lib/api_construct-stack.ts
+++ API/apiConstruct/lib/api_construct-stack.ts
@@ -133,8 +133,115 @@ export class ApiConstructStack extends Stack {
     // )
     // UsagePlan_API_vbs_issue_handle.addApiKey(Key_vbs_issue_handle);
 
+    //#4 Iam //////////////////////////////
+
+  
+
+    const Function_vbs_create_iam_user = new lambda.DockerImageFunction(this, 'Function_vbs_create_iam_user',{
+      functionName: 'Function_vbs_create_iam_user',
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../src/iamManage'), {
+      cmd: [ "app.lambda_handler" ],
+      }),
+      timeout: Duration.seconds(900),
+  });
+    const Policy_vbs_create_iam_user = new iam.PolicyStatement();
+    Policy_vbs_create_iam_user.addResources("*");
+    Policy_vbs_create_iam_user.addActions("*");
+    Function_vbs_create_iam_user.addToRolePolicy(Policy_vbs_create_iam_user);
+
+    const eventRule=new events.Rule(this, "twentyDaysRule", {
+    
+      schedule:events.Schedule.rate(cdk.Duration.days(20))
+    });
+
+    eventRule.addTarget(
+      new targets.LambdaFunction(Function_vbs_create_iam_user, {
+        event: events.RuleTargetInput.fromObject({ message: "Hello Lambda" }),
+      })
+    );
+
+    targets.addLambdaPermission(eventRule, Function_vbs_create_iam_user);
+
+
 
     
+    ///////////life Cycle
+    const Function_vbs_lifecycle_manage = new lambda.DockerImageFunction(this, 'Function_vbs_lifecycle_manage',{
+      functionName: 'Function_vbs_lifecycle_manage',
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../src/lifeCycleCost'), {
+      cmd: [ "app.lambda_handler" ],
+      }),
+      timeout: Duration.seconds(900),
+  });
+    const Policy_vbs_lifecycle_manage = new iam.PolicyStatement();
+    Policy_vbs_lifecycle_manage.addResources("*");
+    Policy_vbs_lifecycle_manage.addActions("*");
+    Function_vbs_lifecycle_manage.addToRolePolicy(Policy_vbs_lifecycle_manage);
+
+    const eventRule_vbs_lifecycle_manage=new events.Rule(this, "twentyDaysRule", {
+    
+      schedule:events.Schedule.rate(cdk.Duration.minutes(5))
+    });
+
+    eventRule_vbs_lifecycle_manage.addTarget(
+      new targets.LambdaFunction(Function_vbs_lifecycle_manage, {
+        event: events.RuleTargetInput.fromObject({ message: "Hello Lambda" }),
+      })
+    );
+
+    targets.addLambdaPermission(eventRule_vbs_lifecycle_manage, Function_vbs_lifecycle_manage);
+
+
+    //////////////# Network ###############
+    const Function_vbs_network_analysis = new lambda.DockerImageFunction(this, 'Function_vbs_network_analysis',{
+      functionName: 'Function_vbs_network_analysis',
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../src/network'), {
+      cmd: [ "app.lambda_handler" ],
+    
+     
+      }),
+      timeout: Duration.seconds(900),
+  });
+
+    const Policy_vbs_network_analysis = new iam.PolicyStatement();
+    Policy_vbs_network_analysis.addResources("*");
+    Policy_vbs_network_analysis.addActions("*");
+    Function_vbs_network_analysis.addToRolePolicy(Policy_vbs_network_analysis);
+    const API_vbs_network_analysis=new apigateway.LambdaRestApi(this, 'API_vbs_network_analysis', {
+      handler: Function_vbs_network_analysis,
+      restApiName:'API_vbs_network_analysis',
+      proxy: false,
+      apiKeySourceType:ApiKeySourceType.HEADER,
+      defaultCorsPreflightOptions: { 
+        allowHeaders: [
+          'Content-Type',
+          'X-Amz-Date',
+          'Authorization',
+          'X-Api-Key',
+        ],
+        allowOrigins: apigateway.Cors.ALL_ORIGINS },
+      integrationOptions: {
+      allowTestInvoke: false,
+        timeout: Duration.seconds(29),
+      }
+    });
+
+    const API_vbs_network_analysis_v1 = API_vbs_network_analysis.root.addResource('v1');
+    const API_vbs_network_analysis_user = API_vbs_network_analysis_v1.addResource('user');
+    const API_vbs_network_analysis_userid = API_vbs_network_analysis_user.addResource('{userid}');
+    
+    const API_vbs_network_analysis_ec2 = API_vbs_network_analysis_userid.addResource("ec2")
+    const API_vbs_network_analysis_ec2id = API_vbs_network_analysis_ec2.addResource('{ec2id}')
+
+    const Authorizer_vbs_network_analysis = new apigateway.RequestAuthorizer(this, 'Authorizer_vbs_network_analysis', {
+      handler: Function_vbs_api_authorize,
+      identitySources: [apigateway.IdentitySource.header('Authorization')]
+    });
+    API_vbs_network_analysis_ec2id.addMethod('POST',
+    new apigateway.LambdaIntegration(Function_vbs_network_analysis, {proxy: true}), {
+      authorizer: Authorizer_vbs_network_analysis
+    });
+
   
   
   }
